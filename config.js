81e5914c1375e822cb888024f19de7146b308611
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2025-10-22 15:43:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -30,10 +30,10 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "87.106.62.92", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 11642,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
-                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "pnode1.danbot.host", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 1351,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "cocaine", 
+                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'false' || false
             },
             
             /**
@@ -101,4 +101,5 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
