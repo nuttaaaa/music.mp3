104d3ed627e945fcbe8b96254831a3dce155a574
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2025-11-06 14:35:19 -0800

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ class EnterpriseConfigurationManager {
                 prefix: EnvironmentVariableProcessor.BOT_PREFIX || "!",  // 👈 prefix (!, ?, etc)
                 ownerIds: ["1004206704994566164"],      // 👈 ADD YOUR DISCORD ID HERE
                 embedColor: 0x00AE86,               // 👈 Bot embed color (hex)
-                supportServer: "https://discord.gg/xQF9f9yUEM",    // 👈 Your support server link
+                supportServer: "https://discord.gg/WZN5ARrmbX",    // 👈 Your support server link
                 defaultStatus: "🎵 Ready for music!"         // 👈 Bot status message
             },
             
@@ -101,5 +101,6 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
