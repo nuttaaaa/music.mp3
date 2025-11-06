ef93075c82f06fc2563034383f30a7f25d933b87
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2025-11-06 14:34:01 -0800

    Update support.js

--- commands/message/support.js
+++ commands/message/support.js
@@ -25,9 +25,7 @@ module.exports = {
                 .setColor(0x1DB954)
                 .setDescription(
                     'Need help or have questions? Join our official support server:\n' +
-                    '[Support Server](https://discord.gg/xQF9f9yUEM)\n\n' +
-                    'For direct inquiries, contact: **GlaceYT**\n\n' +
-                    'Website: https://glaceyt.com'
+                    '[Support Server](https://discord.gg/WZN5ARrmbX)\n\n' +
                 )
                 .setTimestamp()
             
@@ -38,4 +36,5 @@ module.exports = {
         }
     }
 };
+
 
