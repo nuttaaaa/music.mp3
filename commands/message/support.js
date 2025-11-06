5442ade7d023849b782a6688da00c5c19939d8e9
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2025-11-06 14:42:58 -0800

    Update support.js

--- commands/message/support.js
+++ commands/message/support.js
@@ -25,7 +25,8 @@ module.exports = {
                 .setColor(0x1DB954)
                 .setDescription(
                     'Need help or have questions? Join our official support server:\n' +
-                    '[Support Server](https://discord.gg/WZN5ARrmbX)\n\n' + )
+                    '[Support Server](https://discord.gg/WZN5ARrmbX)\n\n' + 
+                )
                 .setTimestamp()
             
             await message.reply({ embeds: [embed] });
@@ -35,6 +36,7 @@ module.exports = {
         }
     }
 };
+
 
 
 
